95649318c0cc62f3ff596838030a93aa3268319c
poojan009p <poojanwork101@gmail.com>
2026-06-08 09:41:26 +0530

    for-loop

---
+++ for loop.py
@@ -0,0 +1,10 @@
+"""
+Level 1 → Basic loops
+Level 2 → Loops + if statements
+Level 3 → Lists and strings
+Level 4 → Nested loops
+Level 5 → Real-world data problems
+Level 6 → NumPy/Pandas operations
+"""
+
+"---------------------------------------------------------"
