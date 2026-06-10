c06843a26b192a693a46b3815a5e06e4b9d77dd0
poojan009p <poojanwork101@gmail.com>
2026-06-10 09:14:27 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,26 @@
+"""
+1) What is a Function?
+
+A function is a block of code that performs a specific task.
+
+Instead of writing the same code repeatedly, you write it once and call it whenever needed.
+
+"""
+#-----------------------------------------------------------------------------------------------------------------------------"
+
+
+def greet(name = "Poojan"):
+    print(f"Hello how are you {name}! ")
+
+# greet()
+
+
+# # function syntax
+# def function_name():
+#     code
+
+
+# return statement
+def add(a, b):
+    return a + b
+
